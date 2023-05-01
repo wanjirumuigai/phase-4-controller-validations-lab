f420dae09f0bfafa8ff17cf5f6f99c960919fb61
Pauline Muigai <wanjirumuigai@gmail.com>
2023-05-01 14:21:57 +0300

    completed lab

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,30 @@
 class PostsController < ApplicationController
-
+  rescue_from ActiveRecord::RecordInvalid,
+              with: :render_unprocessable_entity_response
   def show
     post = Post.find(params[:id])
-    
+
     render json: post
   end
 
   def update
     post = Post.find(params[:id])
 
-    post.update(post_params)
+    post.update!(post_params)
 
     render json: post
   end
 
   private
 
+  def render_unprocessable_entity_response(invalid)
+    render json: {
+             errors: invalid.record.errors
+           },
+           status: :unprocessable_entity
+  end
+
   def post_params
     params.permit(:category, :content, :title)
   end
-
 end
